46d9b940f960976536e8303858deb2f92dc3492e
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-04-10 10:07:54 +0100

    initial rails app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BIPIndicatorsPage::Application.config.secret_token = '6a00b3e5f1400071650357f78d93e358f198326f4aa4c350622b0466af85e697a49c066ae88e594a1ae0b63e5dee5572d2f3c4d71cecbff63384cb27e87bc19f'
