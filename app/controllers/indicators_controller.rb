76fe3b0c138c672e49585eb2bc7a1abd0d21410a
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-04-10 12:26:14 +0100

    Main default action

---
+++ app/controllers/indicators_controller.rb
@@ -0,0 +1,4 @@
+class IndicatorsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 BIPIndicatorsPage::Application.routes.draw do
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +49,7 @@ BIPIndicatorsPage::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'indicators#index'
 
   # See how all your routes lay out with "rake routes"
 
