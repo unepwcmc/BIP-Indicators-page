53fe8091e3a6e7cc78f4577f2c69bbc2a83fc3bd
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-04-10 12:54:07 +0100

    Pass all information from the server to the client

--- app/controllers/indicators_controller.rb
+++ app/controllers/indicators_controller.rb
@@ -1,5 +1,9 @@
 class IndicatorsController < ApplicationController
   def index
     @indicators = Indicator.all
+    @goals = Goal.all
+    @targets = Target.all
+    @headlines = Headline.all
+    @partners = Partner.all
   end
 end
