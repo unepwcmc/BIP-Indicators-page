aec0b192e9336231b740e3319736772aaa09c0e2
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-04-16 08:15:24 +0100

    removed unused route

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,6 @@ BIPIndicatorsPage::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
-  match 'timestamp' => 'indicators#timestamp'
-
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
