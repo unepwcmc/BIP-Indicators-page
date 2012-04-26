a72d19c92fbd7c17fc864d8884f2ad7268f6e201
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2012-04-26 14:10:35 +0100

    changed db schema to accommodate for multiple headlines per indicator

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120416122039) do
+ActiveRecord::Schema.define(:version => 20120426130049) do
 
   create_table "focal_areas", :force => true do |t|
     t.string   "name"
@@ -39,6 +39,11 @@ ActiveRecord::Schema.define(:version => 20120416122039) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "code"
+  end
+
+  create_table "headlines_indicators", :id => false, :force => true do |t|
+    t.integer "headline_id"
+    t.integer "indicator_id"
   end
 
   create_table "indicators", :force => true do |t|
