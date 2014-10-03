be4fef750dc6b8227a2ae26069f758f19b219ab2
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-10-03 16:20:27 +0100

    new production box

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,10 +4,10 @@ require 'brightbox/recipes'
 require 'brightbox/passenger'
 
 # Primary domain name of your application. Used in the Apache configs
-set :domain, "unepwcmc-004.vm.brightbox.net"
+set :domain, "unepwcmc-013.vm.brightbox.net"
 
 ## List of servers
-server "unepwcmc-004.vm.brightbox.net", :app, :web, :db, :primary => true
+server "unepwcmc-013.vm.brightbox.net", :app, :web, :db, :primary => true
 
 set :branch, "master"
 
